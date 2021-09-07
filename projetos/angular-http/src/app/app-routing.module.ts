c6f5d4d854128e1a2f01e26aff829a45728842cd
Wallace <wallace_wolv@hotmail.com>
2021-09-07 01:02:04 -0300

    Created car.service

--- projetos/angular-http/src/app/app-routing.module.ts
+++ projetos/angular-http/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+// const routes:Routes [
+//   {component: FundosAplicarComponent, path: 'fundos-aplicar', outlet: 'appOutlet'}
+// ]
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    // RouterModule.forRoot(routes),
+  ],
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
